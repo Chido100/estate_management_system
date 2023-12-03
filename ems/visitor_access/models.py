55bbf1a93fae3039da56d1d0a49b0fb636d36c67
Chido100 <chido_dozie@hotmail.com>
2023-12-03 18:00:40 +0000

    Created visitor access model

--- ems/visitor_access/models.py
+++ ems/visitor_access/models.py
@@ -1,3 +1,37 @@
 from django.db import models
+import uuid
+from users.models import User
 
-# Create your models here.
+
+
+STATUS_CHOICES = (
+    ('active', 'Active'),
+    ('completed', 'Completed'),
+    ('pending', 'Pending')
+)
+
+GENDER_CHOICES = (
+    ('', 'Select gender'),
+    ('male', 'Male'),
+    ('female', 'Female')
+)
+
+class VisitorAccess(models.Model):
+    ticket_number = models.UUIDField(default=uuid.uuid4)
+    visitor_name = models.CharField(max_length=100)
+    gender = models.CharField(max_length=20, choices=GENDER_CHOICES)
+    vehicle_registration = models.CharField(max_length=20, blank=True, null=True)
+    additional_information = models.TextField()
+    creator = models.ForeignKey(User, on_delete=models.CASCADE, related_name='creator')
+    date_created = models.DateTimeField(auto_now_add=True)
+    assigned_to = models.ForeignKey(User, on_delete=models.DO_NOTHING, null=True, blank=True)
+    is_resolved = models.BooleanField(default=False)
+    accepted_date = models.DateTimeField(null=True, blank=True)
+    closed_date = models.DateTimeField(null=True, blank=True)
+    ticket_status = models.CharField(max_length=50, choices=STATUS_CHOICES)
+
+    class Meta:
+        verbose_name_plural = 'Visitor Access'
+
+    def __str__(self):
+        return self.creator
