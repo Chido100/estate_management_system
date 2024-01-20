926268767640cc4c9e5d970102a53b55ae23bc7e
Chido100 <chido_dozie@hotmail.com>
2024-01-20 12:40:36 +0000

    Added decouple

--- ems/ems/settings.py
+++ ems/ems/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 import os
 from pathlib import Path
 
+from decouple import config
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,10 +23,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-%!f$8qz#x)27qdnnljp_@hx+(69!a%$fqn_-j$v88t%z%j8-#-'
+SECRET_KEY = config('THE_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('MY_DEBUG', cast=bool)
 
 ALLOWED_HOSTS = []
 
@@ -32,9 +34,6 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-
-    #'jazzmin',
-
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -117,7 +116,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'en-gb'
 
 TIME_ZONE = 'UTC'
 
@@ -157,7 +156,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_FROM = 'chidozieamefule@googlemail.com'
 EMAIL_HOST_USER = 'chidozieamefule@googlemail.com'
-EMAIL_HOST_PASSWORD = 'xhsq lsru vqze zxsd'
+EMAIL_HOST_PASSWORD = config('MY_EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
@@ -165,8 +164,3 @@ EMAIL_USE_TLS = True
 PASSWORD_RESET_TIMEOUT = 14400
 
 
-
-#JAZZMIN_SETTINGS = {
-#    'site_brand': 'EMS',
-#    'site_title': 'EMS',
-#}
